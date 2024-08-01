5451c69cd6ebedf76019e9e2893d5d90767dc2f5
Garrett Lander <garrett.jlander@gmail.com>
2024-08-01 10:49:26 +0200

    can also decrypt caesarCypher now

--- week2_web_apis/command_line_word_games/caesarCipher.js
+++ week2_web_apis/command_line_word_games/caesarCipher.js
@@ -1,8 +1,8 @@
 const args = process.argv.slice(2);
 
-if (args.length !== 2) {
+if (args.length !== 2 && args.length !== 3) {
     return console.error(
-        'Please enter your phrase surrounded by quotes, followed by a number'
+        'Please enter your phrase surrounded by quotes, followed by a number. If you want to decode,  write "decode" as a third argument.'
     );
 }
 const phraseArray = args[0].split(' ');
@@ -10,6 +10,10 @@ const shift = parseFloat(args[1]);
 
 if (isNaN(shift)) {
     console.error('Second argument must be a number');
+}
+
+if (args.length === 3 && args[2].toLowerCase() !== 'decrypt') {
+    console.error('Third argument must say "decrypt"');
 }
 // console.log(phraseArray);
 const alphabet = [
@@ -40,26 +44,52 @@ const alphabet = [
     'y',
     'z',
 ];
-const shiftedPhraseArray = phraseArray.map((word) => {
+
+if (args.length === 2) {
+    const shiftedPhraseArray = phraseArray.map((word) => {
+        const wordArray = word.toLowerCase().split('');
+        // console.log(wordArray);
+        const shiftedWordArray = wordArray.map((letter) => {
+            const i = alphabet.findIndex((alpha) => alpha === letter);
+            const shiftedI = i + shift;
+            const remainder = shiftedI - (alphabet.length - 1);
+            // console.log('shiftedI', shiftedI);
+            // console.log('remainder', remainder);
+            // return alphabet[shiftedI];
+            if (shiftedI < 0) {
+                return alphabet[alphabet.length + shiftedI];
+            } else if (remainder > 0) {
+                return alphabet[remainder - 1];
+            } else {
+                return alphabet[shiftedI];
+            }
+        });
+        // console.log(shiftedArray);
+        return shiftedWordArray.join('');
+    });
+
+    return console.log(shiftedPhraseArray.join(' '));
+}
+
+const decodedPhraseArray = phraseArray.map((word) => {
     const wordArray = word.toLowerCase().split('');
     // console.log(wordArray);
-    const shiftedWordArray = wordArray.map((letter) => {
+    const decodedWordArray = wordArray.map((letter) => {
         const i = alphabet.findIndex((alpha) => alpha === letter);
-        const shiftedI = i + shift;
-        const remainder = shiftedI - (alphabet.length - 1);
+        const decodedI = i - shift;
+        const remainder = decodedI - (alphabet.length - 1);
         // console.log('shiftedI', shiftedI);
         // console.log('remainder', remainder);
         // return alphabet[shiftedI];
-        if (shiftedI < 0) {
-            return alphabet[alphabet.length + shiftedI];
+        if (decodedI < 0) {
+            return alphabet[alphabet.length + decodedI];
         } else if (remainder > 0) {
             return alphabet[remainder - 1];
         } else {
-            return alphabet[shiftedI];
+            return alphabet[decodedI];
         }
     });
     // console.log(shiftedArray);
-    return shiftedWordArray.join('');
+    return decodedWordArray.join('');
 });
-
-console.log(shiftedPhraseArray.join(' '));
+console.log(decodedPhraseArray.join(' '));
