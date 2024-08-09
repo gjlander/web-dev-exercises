959dbe389198207b83d629d5a85a9632dc22efa4
Garrett Lander <garrett.jlander@gmail.com>
2024-08-09 18:36:40 +0200

    working on localstorae

--- week2_web_apis/web_storage_api_exercises/web_apis__storage_exercises_more...complex_data/script.js
+++ week2_web_apis/web_storage_api_exercises/web_apis__storage_exercises_more...complex_data/script.js
@@ -1,1 +1,49 @@
-// You can work here or download the template
+const form = document.querySelector('form');
+const userInput = document.querySelector('#userInput');
+const ul = document.querySelector('ul');
+const reloadBtn = document.querySelector('#reload');
+
+const quotes = JSON.parse(localStorage.getItem('quotes')) || [];
+
+const makeNewLi = (quote) => {
+    const newLi = document.createElement('li');
+    const newP = document.createElement('p');
+    const newBtn = document.createElement('button');
+
+    newLi.id = quote.id;
+    newLi.classList.add('flex', 'gap-4');
+
+    newBtn.textContent = 'Delete';
+    newP.textContent = quote.content;
+
+    newLi.appendChild(newP);
+    newLi.appendChild(newBtn);
+    return newLi;
+};
+const renderStorage = () => {
+    quotes.forEach((q) => {
+        const newLi = makeNewLi(q);
+        ul.appendChild(newLi);
+    });
+};
+renderStorage();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!userInput.value) return alert('Cannot submit an empty quote!');
+    const newQuote = {
+        id: 'task-' + crypto.randomUUID(),
+        content: userInput.value,
+    };
+    quotes.unshift(newQuote);
+    localStorage.setItem('quotes', JSON.stringify(quotes));
+    const newLi = document.createElement('li');
+    newLi.id = newQuote.id;
+    newLi.textContent = userInput.value;
+    ul.insertBefore(newLi, ul.firstChild);
+    form.reset();
+});
+
+reloadBtn.addEventListener('click', () => {
+    window.location.reload();
+});
--- week2_web_apis/web_storage_api_exercises/web_apis__storage_exercises_store_and_retrieve_data/script.js
+++ week2_web_apis/web_storage_api_exercises/web_apis__storage_exercises_store_and_retrieve_data/script.js
@@ -1,1 +1,29 @@
-// You can work here or download the template
+const form = document.querySelector('form');
+const userInput = document.querySelector('#userInput');
+const ul = document.querySelector('ul');
+const reloadBtn = document.querySelector('#reload');
+
+const quotes = JSON.parse(localStorage.getItem('quotes')) || [];
+const renderStorage = () => {
+    quotes.forEach((q) => {
+        const newLi = document.createElement('li');
+        newLi.textContent = q;
+        ul.appendChild(newLi);
+    });
+};
+renderStorage();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!userInput.value) return alert('Cannot submit an empty quote!');
+    quotes.unshift(userInput.value);
+    localStorage.setItem('quotes', JSON.stringify(quotes));
+    const newLi = document.createElement('li');
+    newLi.textContent = userInput.value;
+    ul.insertBefore(newLi, ul.firstChild);
+    form.reset();
+});
+
+reloadBtn.addEventListener('click', () => {
+    window.location.reload();
+});
