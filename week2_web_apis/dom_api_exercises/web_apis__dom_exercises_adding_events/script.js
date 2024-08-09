8b3cbd12227219542c0ae5e8fb99fda8fce1563b
Garrett Lander <garrett.jlander@gmail.com>
2024-08-09 17:16:30 +0200

    form exercise completed

--- week2_web_apis/dom_api_exercises/web_apis__dom_exercises_adding_events/script.js
+++ week2_web_apis/dom_api_exercises/web_apis__dom_exercises_adding_events/script.js
@@ -1,14 +1,34 @@
 // You can work here or download the template
 // Array of 10 random tasks as strings
 const tasks = [
-  'Complete the project',
-  'Attend the meeting',
-  'Write a report',
-  'Review the code',
-  'Fix the bugs',
-  'Update the documentation',
-  'Plan the next sprint',
-  'Conduct user testing',
-  'Optimize the performance',
-  'Design',
+    'Complete the project',
+    'Attend the meeting',
+    'Write a report',
+    'Review the code',
+    'Fix the bugs',
+    'Update the documentation',
+    'Plan the next sprint',
+    'Conduct user testing',
+    'Optimize the performance',
+    'Design',
 ];
+const addBtn = document.getElementById('add-item-btn');
+const alertBtn = document.getElementById('alert-btn');
+const logBtn = document.getElementById('console-btn');
+const itemList = document.getElementById('item-list');
+
+let currentTask = 0;
+addBtn.addEventListener('click', () => {
+    const newTask = document.createElement('li');
+    newTask.textContent = tasks[currentTask];
+    itemList.appendChild(newTask);
+    newTask.scrollIntoView();
+    currentTask++;
+});
+
+alertBtn.addEventListener('click', () => {
+    alert('Do you feel alert?');
+});
+logBtn.addEventListener('click', () => {
+    console.log('Hehe, butts');
+});
--- week2_web_apis/dom_api_exercises/web_apis__dom_exercises_submitting_forms/script.js
+++ week2_web_apis/dom_api_exercises/web_apis__dom_exercises_submitting_forms/script.js
@@ -1,1 +1,25 @@
 // You can work here or download the template
+const contactForm = document.querySelector('#contact-form');
+const nameInput = document.querySelector('#name');
+const messageInput = document.querySelector('#message');
+const emailInput = document.querySelector('#email');
+
+const output = document.querySelector('#output');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!nameInput.value || !messageInput.value || !emailInput.value) {
+        output.classList.remove('bg-green-500');
+        output.classList.add('bg-red-500');
+        output.textContent = 'Please fill out all fields!';
+    } else {
+        output.innerHTML = `<ul>
+        <li>${nameInput.value}</li>
+        <li>${emailInput.value}</li>
+        <li>${messageInput.value}</li>
+        </ul>`;
+        output.classList.remove('bg-red-500');
+        output.classList.add('bg-green-500');
+    }
+    contactForm.reset();
+});
