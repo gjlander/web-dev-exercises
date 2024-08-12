317cba38adae727dafe5c70b84b90051f1fc982e
Garrett Lander <garrett.jlander@gmail.com>
2024-08-12 14:28:39 +0200

    first fetch exercise done

--- week2_web_apis/fetch_api_exercises/web_apis__fetch_exercises_get_data_and_add_to_dom/script.js
+++ week2_web_apis/fetch_api_exercises/web_apis__fetch_exercises_get_data_and_add_to_dom/script.js
@@ -1,1 +1,21 @@
-// You can work here or download the template
+const todoList = document.querySelector('#todo-list');
+
+const fetchAndRenderTodos = async () => {
+    const res = await fetch(
+        'https://jsonplaceholder.typicode.com/todos?limit=20'
+    );
+    const data = await res.json();
+    console.log(data);
+
+    data.forEach((todo) => {
+        const li = document.createElement('li');
+        li.setAttribute('id', todo.id);
+        li.textContent = todo.title;
+        todo.completed
+            ? li.classList.add('bg-green-500', 'border-2')
+            : li.classList.add('bg-orange-500', 'border-2');
+        todoList.appendChild(li);
+    });
+};
+
+fetchAndRenderTodos();
