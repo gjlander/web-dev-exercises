a3fc37d69a26065e965af5bc10be04e75b741dc9
Garrett Lander <garrett.jlander@gmail.com>
2024-08-19 12:20:25 +0200

    finished usestate exercises, copied thinking in react to part 2

--- week4_intro_to_react/useState/react_hooks__usestate_exercises_contact_form/src/App.jsx
+++ week4_intro_to_react/useState/react_hooks__usestate_exercises_contact_form/src/App.jsx
@@ -1,8 +1,85 @@
 // You can work here or download the template
 // Your components go here
 
+import { useState } from 'react';
+
 const App = () => {
-  return <div>{/* Your UI goes here */}</div>;
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        tel: '',
+        message: '',
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!form.name || !form.email || !form.tel || !form.message)
+            return alert('Please fill out all fields!');
+        console.log(form);
+        setForm({
+            name: '',
+            email: '',
+            tel: '',
+            message: '',
+        });
+    };
+    const handleChange = (e) => {
+        setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+    return (
+        <div>
+            <form
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '1rem',
+                    width: '25%',
+                }}
+                onSubmit={handleSubmit}
+            >
+                <label htmlFor='name'>
+                    Name:
+                    <input
+                        value={form.name}
+                        onChange={handleChange}
+                        type='text'
+                        id='name'
+                        name='name'
+                    />
+                </label>
+                <label htmlFor='email'>
+                    Email:
+                    <input
+                        value={form.email}
+                        onChange={handleChange}
+                        type='email'
+                        id='email'
+                        name='email'
+                    />
+                </label>
+                <label htmlFor='tel'>
+                    Telephone:
+                    <input
+                        value={form.tel}
+                        onChange={handleChange}
+                        type='tel'
+                        id='tel'
+                        name='tel'
+                    />
+                </label>
+                <label htmlFor='message'>
+                    Message:
+                    <textarea
+                        value={form.message}
+                        onChange={handleChange}
+                        name='message'
+                        id='message'
+                    ></textarea>
+                </label>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    );
 };
 
 export default App;
