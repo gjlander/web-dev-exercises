79782ae6beee1ccb1c14f33e175a727e0e7c008e
Garrett Lander <garrett.jlander@gmail.com>
2024-08-20 17:13:33 +0200

    candle exercise done

--- week4_intro_to_react/useEffect/react_hooks__useeffect_exercises_the_candle/src/App.jsx
+++ week4_intro_to_react/useEffect/react_hooks__useeffect_exercises_the_candle/src/App.jsx
@@ -1,25 +1,40 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 // You can work here or download the template
 const Candle = () => {
-  return (
-    <div className="exercise">
-      <div className="candleContainer">
-        <div className="candle">
-          <div className="flame">
-            <div className="shadows" />
-            <div className="top" />
-            <div className="middle" />
-            <div className="bottom" />
-          </div>
-          <div className="wick" />
-          <div className="wax" />
+    const [height, setHeight] = useState(90);
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (height <= 10) {
+                return setHeight(90);
+            }
+            setHeight((prev) => (prev -= 10));
+            // console.log(height);
+
+            return clearTimeout(timeoutId);
+        }, 500);
+    }, [height]);
+    return (
+        <div className='exercise'>
+            <div className='candleContainer'>
+                <div className='candle' style={{ height: `${height}%` }}>
+                    <div className='flame'>
+                        <div className='shadows' />
+                        <div className='top' />
+                        <div className='middle' />
+                        <div className='bottom' />
+                    </div>
+                    <div className='wick' />
+                    <div className='wax' />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 const App = () => {
-  return <Candle />;
+    return <Candle />;
 };
 
 export default App;
