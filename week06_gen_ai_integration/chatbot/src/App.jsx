c9cee43fb96f309292ef2900de241f0bb2a352ad
Garrett Lander <garrett.jlander@gmail.com>
2024-09-10 10:57:46 +0200

    replaced with template

---
+++ week06_gen_ai_integration/chatbot/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className='hero bg-base-200 min-h-screen'>
+            <div className='hero-content text-center'>
+                <div className='max-w-md'>
+                    <h1 className='text-5xl font-bold'>
+                        Welcome to your Vite+Tailwind+DaisyUI Boilerplate
+                    </h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
