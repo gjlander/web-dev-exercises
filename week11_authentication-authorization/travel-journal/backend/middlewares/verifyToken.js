3aaa5959fde3b94f56dbd84943864c6262f98ec5
Garrett Lander <garrett.jlander@gmail.com>
2024-09-25 18:43:22 +0200

    started on travel journal auth

---
+++ week11_authentication-authorization/travel-journal/backend/middlewares/verifyToken.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from '../utils/asyncHandler.js';
+
+const verifyToken = asyncHandler(async (req, res, next) => {
+    // console.log(req.headers.cookie);
+
+    // const cookies =
+    //     req.headers.cookie?.split(';').map((cookie) => cookie.trim()) || [];
+    // console.log(cookies);
+
+    // const cookiesObj = cookies.reduce((acc, cookie) => {
+    //     const [key, value] = cookie.split('=');
+    //     acc[key] = value;
+    //     return acc;
+    // }, {});
+
+    // const token = cookiesObj.token;
+
+    const [_, token] = req.headers.cookie.split('=');
+
+    // console.log(token);
+
+    if (!token)
+        return res.status(401).json({ error: 'Unauthorized. Please sign in' });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.userId;
+
+    next();
+});
+
+export default verifyToken;
---
+++ week11_authentication-authorization/travel-journal/backend/routes/authRouter.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import validateJOI from '../middlewares/validateJOI.js';
+import verifyToken from '../middlewares/verifyToken.js';
+import { signUp, signIn, me } from '../controllers/auth.js';
+import { userSchema, siginSchema } from '../joi/schemas.js';
+
+const authRouter = Router();
+
+authRouter.route('/signup').post(validateJOI(userSchema), signUp);
+
+authRouter.route('/signin').post(validateJOI(siginSchema), signIn);
+
+authRouter.route('/me').get(verifyToken, me);
+
+export default authRouter;
