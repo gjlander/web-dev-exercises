83dee4c4aac9e6b24b5b529fb9c7663b54482de9
Garrett Lander <garrett.jlander@gmail.com>
2024-09-26 11:52:10 +0200

    auth paths give successful responses

--- week11_authentication-authorization/travel-journal/frontend/src/data/index.js
+++ week11_authentication-authorization/travel-journal/frontend/src/data/index.js
@@ -1,3 +1,4 @@
 import { createPost, getPosts, getSinglePost } from './posts';
+import { me, signin, signup } from './auth';
 
-export { createPost, getPosts, getSinglePost };
+export { createPost, getPosts, getSinglePost, me, signin, signup };
