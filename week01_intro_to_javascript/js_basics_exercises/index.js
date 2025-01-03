a3d095ee317fbe4d31342c7b0777b960a4a05cf1
Garrett Lander <garrett.jlander@gmail.com>
2025-01-03 14:02:42 +0100

    bunch of changes, oops

--- week01_intro_to_javascript/js_basics_exercises/index.js
+++ week01_intro_to_javascript/js_basics_exercises/index.js
@@ -173,12 +173,38 @@ const arrowDayName = (num) => {
 
 //Variables and Scope
 // 1. Global and Function Scope with var:
-var globalVar = 'This is global var.';
-console.log(globalVar);
-
-function testScope() {
-    var globalVar = 'This is func var.';
-    console.log(globalVar);
+var myVar = 'Global with var';
+function test() {
+    // var, let and const behave very similar whtn it comes to function scope
+    var myVar = 'Function scoped with var';
+    console.log(myVar);
 }
-testScope();
-console.log(globalVar);
+
+test();
+
+console.log(myVar);
+
+//2. Block Scope with let and const:
+//3. Redeclaration with var:
+
+if (true) {
+    let myLetVar = 'something';
+    const myConstVar = 'something else';
+    var myVar = 're-declared';
+    // let myLetVar = 'something';
+    // const myConstVar = 'something else';
+    console.log(myLetVar);
+    console.log(myConstVar);
+    console.log(myVar);
+}
+// let and const are scoped to the blocks they are declared in
+// console.log(myLetVar);
+// console.log(myConstVar);
+console.log(myVar);
+
+//4. Usage of const for Arrays and Objects:
+
+const myArray = [1, 2, 3];
+console.log(myArray);
+myArray.push(4);
+console.log(myArray);
