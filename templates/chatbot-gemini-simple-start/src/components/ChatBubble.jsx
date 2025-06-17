5856fcde8b1b20f881a042e4765ab507f8d3081b
Garrett Lander <garrett.jlander@gmail.com>
2025-06-17 12:14:19 +0200

    added markdown support

--- templates/chatbot-gemini-simple-start/src/components/ChatBubble.jsx
+++ templates/chatbot-gemini-simple-start/src/components/ChatBubble.jsx
@@ -1,10 +1,14 @@
+import Markdown from 'react-markdown';
+
 const ChatBubble = () => {
   return (
     <div className='chat chat-end'>
       <div className='chat-image avatar'>
         <div className='w-10 rounded-full p-2 bg-slate-800'>You</div>
       </div>
-      <div className={'chat-bubble chat-bubble-primary'}>Some sample text</div>
+      <div className={'chat-bubble chat-bubble-primary'}>
+        <Markdown>Some ***sample*** text with *Markdown*</Markdown>
+      </div>
     </div>
   );
 };
